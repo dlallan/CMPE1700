17729c6cca03cee8cf216e6c30e4a434722d9080
dlallan <dlallan@users.noreply.github.com>
2015-09-16 16:02:01 -0600

    Final Coding
    Test before submission.

--- Demo 1/Demo 1/Program.cs
+++ Demo 1/Demo 1/Program.cs
@@ -16,18 +16,50 @@ namespace Demo_1
         ***********************************************************/
         static void Main(string[] args)
         {
+
+            uint val = 0; //Value I am generating prime factors for.
             //Confirm I have one argument (exactly)
-            //Confirm that it is a positive integer
-            //Generate facors (if possible)
             if (args.Count() != 1)
             {
                 //Print error and exit
                 PrintError("Invalid number of arguments (" + args.Count() + ")", "", true, true, -1);
             }
-            //for testing.
-            Console.ReadKey();
+
+            //Confirm that it is a positive integer.
+            try
+            {
+                val = uint.Parse(args[0]);
+            }
+            catch (Exception e)
+            {
+
+                PrintError("Parsing Error on argument \"" + args[0] + "\"", e.Message, true, true, -2);
+            }
+
+            if (val < 1)
+                PrintError("Zero is nto a permitted input argument", "", true, true, -3);
+
+            //Generate facors (if possible)
+            //If I got here, I got a positive integer, currently stored in val.
+            Factor(val);
         }
 
+        static void Factor (uint val) //prints out factors
+        {
+            if (val == 1) return;
+            for (int i = 2; i <= val/2; ++i) //Potential factors.
+            {
+                //is i a factor?
+                if (val%i == 0) //Found one!
+                {
+                    Console.Write(i + " ");
+                    //val = val / i;
+                }
+            }
+            Console.WriteLine();
+        }
+        
+        //General Print Error message
         static void PrintError(string Err = "Unknown Failure", string Dbg = "", 
                                 bool printUsage = true, bool exit = false, int exitVal = 1)
         {
@@ -45,8 +77,7 @@ namespace Demo_1
                 PrintUsage();
             Console.ForegroundColor = currForeColor;
             Console.BackgroundColor = currBackColor;
-            //for testing.
-            Console.ReadKey();
+            
             if (exit)
                 Environment.Exit(exitVal); //By convention, exit with a value != 0 for error. i.e. if exitVal = 0, program has run without any errors.
            
@@ -57,7 +88,7 @@ namespace Demo_1
         //General usage message.
         static void PrintUsage()
         {
-            Console.WriteLine("Usage: " + System.AppDomain.CurrentDomain.FriendlyName + "<val>" +
+            Console.WriteLine("Usage: " + System.AppDomain.CurrentDomain.FriendlyName + " <val>" +
                                 "where <val> is a \npositive integer greater than zero. Prints a\nspace delimitted" +
                                 " list of primt factors of only \nargument");
 
